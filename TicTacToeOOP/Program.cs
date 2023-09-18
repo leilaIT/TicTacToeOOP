bd6563c5180e17e48633166b72166d2c5b1a03a7
22-0121c <22-0121c@CCL01-06>
2023-09-18 18:11:33 +0800

    Add project files.

---
+++ TicTacToeOOP/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a simple OOP program of Tic Tac Toe.
+
+            //There are two players, the player and the computer.
+
+            //The computer and the player SHOULD use multiple same methods.
+            //Example, if the player logic uses method1 to place moves on the board,
+            //the computer logic should also use method1. (15 exp penalty if failed to accomplish)
+
+            //There should be no logic performed inside the Main method. (15 exp penalty if failed to accomplish)
+
+            //Each game should alternate who goes first. Example, if the first game the player starts,
+            //the second game the computer starts. (5 exp penalty if failed to accomplish)
+
+            //The game ends after a best of 5(first to 3 wins) or after 5 consecutive draws. (5 exp penalty if failed to accomplish)
+
+            //If it can win, the computer must always try to win.
+            //The computer should always do anything within the rules to stop the player from winning. (5 exp penalty if failed to accomplish)
+
+            //BONUS: After every game, a history of moves is written to a file with a unique filename.
+            
+            string[,] board = new string[3,3];
+            string pMove = "";
+            string cpMove = "";
+            string[] moveSplit = new string[] { };
+
+            //initialize board
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    board[x, y] = "-";
+                }
+            }
+
+            //player move
+            Console.WriteLine("Please enter valid coordinates using the following format x-y" +
+                              "\nx is the column number (0-2)" +
+                              "\ny is the row number(0-2)");
+            pMove = Console.ReadLine();
+            moveSplit = pMove.Split('-');
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                for(int y = 0;y < board.GetLength(1); y++)
+                {
+                    board[x, y] = board[moveSplit[0], moveSplit[1]];
+                }
+            }
+
+            //display
+            displayBoard(board);
+            Console.ReadKey();
+        }
+        static string[,] displayBoard(string[,] board)
+        {
+            for (int x = 0; x < board.GetLength(0); x++)
+            {
+                Console.Write("| ");
+                for (int y = 0; y < board.GetLength(1); y++)
+                {
+                    Console.Write(board[x, y] + " | ");
+                }
+                Console.WriteLine("\n-------------");
+            }
+            return board;
+        }
+    }
+}
